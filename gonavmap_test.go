7b45dd1d88948da63a0a33536781485d542371b0
Andreas Louca <andreas@louca.org>
2013-04-08 16:05:33 +0300

    Added license

--- gonavmap_test.go
+++ gonavmap_test.go
@@ -1,3 +1,7 @@
+// Copyright 2013 Andreas Louca. All rights reserved.
+// Use of this source code is goverend by a BSD-style
+// license that can be found in the LICENSE file.
+
 package gonavmap
 
 import (
